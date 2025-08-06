a2c33bfd02211a958f61955d50deb75ba8990d04
Lucas Fernandes <lucas.silva.fernandes.lsf@gmail.com>
2025-08-06 15:23:12 -0300

    criando modelo do Item e do Mundo

--- models.py
+++ models.py
@@ -8,3 +8,22 @@ class Usuario(bd.Model):
 
     def __repr__(self):
         return'<Usuario %r>' % self.nickname
+    
+
+class Item(bd.Model):
+   idItem = bd.Column(bd.Integer, primary_key=True, auto_increment=True)
+   nome = bd.Column(bd.String(25), nullable=False)
+   valor = bd.Column(bd.Integer, nullable=False)
+
+   def __repr__(self):
+        return'<Item %r>' % self.nome
+
+ #idMundo INT PRIMARY KEY AUTO_INCREMENT,  
+ #linguagem INT NOT NULL,  
+ #idModulo INT,  
+ #UNIQUE (linguagem)
+ #FOREIGN KEY(idModulo) REFERENCES Modulo(idModulo)
+
+class Mundo(bd.Model):
+    idMundo = bd.Column(bd.Integer, primary_key=True, auto_increment=True)
+    linguagem = bd.Column(bd.String(8), nullable=False, unique=True)
