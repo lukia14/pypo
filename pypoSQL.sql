93053677fef53528b57f9d9d2c371f565cc921d4
lucas fernandes <lucas.silva.fernandes.lsf@gmail.com>
2025-09-24 20:59:34 -0300

    adicionando bd e requirements

---
+++ pypoSQL.sql
@@ -0,0 +1,84 @@
+drop database if exists pypo;
+create database pypo;
+use pypo;
+CREATE TABLE Usuario 
+( 
+ senha VARCHAR(25) NOT NULL,  
+ idUsuario INT PRIMARY KEY AUTO_INCREMENT,  
+ email VARCHAR(45) NOT NULL,  
+ nickname VARCHAR(25) NOT NULL,  
+ UNIQUE (nickname)
+); 
+
+CREATE TABLE Item 
+( 
+ idItem INT PRIMARY KEY AUTO_INCREMENT,  
+ nome VARCHAR(25) NOT NULL,  
+ valor INT NOT NULL 
+); 
+
+CREATE TABLE Mundo 
+( 
+ idMundo INT PRIMARY KEY AUTO_INCREMENT,  
+ linguagem VARCHAR(8) NOT NULL UNIQUE
+); 
+
+insert into Mundo values (default,"portugol");
+
+CREATE TABLE Modulo 
+( 
+ idModulo INT PRIMARY KEY AUTO_INCREMENT,  
+ numero INT NOT NULL,  
+ nome VARCHAR(25) NOT NULL UNIQUE,
+ idMundo int,
+ foreign key (idMundo) references Mundo(idMundo)
+); 
+
+insert into Modulo values (default,1, "introdução",1);
+
+CREATE TABLE Fase 
+( 
+ idFase INT PRIMARY KEY AUTO_INCREMENT,  
+ MaterialApoio VARCHAR(99) NOT NULL,
+ idModulo INT,  
+ FOREIGN KEY(idModulo) REFERENCES Modulo(idModulo)
+); 
+
+insert into Fase values(default,"Pudim","1");
+
+
+CREATE TABLE Exercicio 
+( 
+ titulo VARCHAR(25) NOT NULL UNIQUE,  
+ Enunciado VARCHAR(99) NOT NULL UNIQUE,  
+ alternativaA VARCHAR(99) NOT NULL,  
+ alternativaB VARCHAR(99) NOT NULL,
+ alternativaC VARCHAR(99) NOT NULL,
+ alternativaD VARCHAR(99) NOT NULL,
+ resposta CHAR(1) NOT NULL,  
+ idExercicio INT PRIMARY KEY AUTO_INCREMENT,  
+ numero INT,  
+ idFase INT,
+ FOREIGN KEY(idFase) REFERENCES Fase(idFase)
+); 
+
+insert into exercicio values("pudim","a","a","a","a","a","a",default,1,1);
+
+CREATE TABLE Estoque 
+( 
+ qtd INT NOT NULL,  
+ idUsuario INT,  
+ idItem INT, 
+ PRIMARY KEY(idUsuario, idItem),
+ FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario),
+ FOREIGN KEY(idItem) REFERENCES Item(idItem)
+); 
+
+CREATE TABLE Progresso 
+( 
+ idUsuario INT,  
+ idFase INT,
+ PRIMARY KEY(idUsuario, idFase),
+ FOREIGN KEY(idUsuario) REFERENCES Usuario(idUsuario),
+ FOREIGN KEY(idFase) REFERENCES Fase(idFase)
+); 
